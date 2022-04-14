d2d3d4ba542d2a36815e6886bdb654409400e47c
Kim Naranjo <rolando.naranjo92@yahoo.com>
2022-04-13 18:50:01 -0700

    init

---
+++ MarsRover.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverConsoleApp
+{
+    public class MarsRover
+    {
+        public int x;
+        public int y;
+        public string direction;
+        public int maxX;
+        public int maxY;
+
+        private Dictionary<string, string> cardinalDictionaryOnLeft = new Dictionary<string, string>() 
+        { 
+            { "N", "W" },
+            { "W", "S" },
+            { "S", "E" },
+            { "E", "N" },
+        };
+
+        private Dictionary<string, string> cardinalDictionaryOnRight = new Dictionary<string, string>()
+        {
+            { "N", "E" },
+            { "W", "N" },
+            { "S", "W" },
+            { "E", "S" },
+        };
+
+        public void InitializePlateau(string plateau)
+        {
+            Int32.TryParse(plateau.Split(" ")[0], out maxX);
+            Int32.TryParse(plateau.Split(" ")[1], out maxY);
+        }
+
+        public void InitializeMarsRoverPos(string position)
+        {
+            if (position.Length > 0)
+            {
+                Int32.TryParse(position.Split(" ")[0], out x);
+                Int32.TryParse(position.Split(" ")[1], out y);
+                direction = position.Split(" ")[2];
+            } else
+            {
+                Console.WriteLine("Unable to initialize values for MarsRover position");
+            }
+        }
+
+        public void Right()
+        {
+            if (cardinalDictionaryOnRight.ContainsKey(direction))
+            {
+                direction = cardinalDictionaryOnRight[direction];
+            }
+            else
+            {
+                Console.WriteLine("Invalid Cardinal Point Right Spin");
+            }
+        }
+
+        public void Left()
+        {
+            if (cardinalDictionaryOnLeft.ContainsKey(direction))
+            {
+                direction = cardinalDictionaryOnLeft[direction];
+            }
+            else
+            {
+                Console.WriteLine("Invalid Cardinal Point for Left Spin");
+            }
+
+        }
+
+        public void StepForward()
+        {
+            switch (direction)
+            {
+                case "N":
+                    if(y < maxY)
+                    {
+                        y++;
+                    } 
+                    else
+                    {
+                        Console.WriteLine("y axis passed the maximum amount. Restarting the Mars Rover App....");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+                case "W":
+                    if(x > 0)
+                    {
+                        x--;
+                    } else
+                    {
+                        Console.WriteLine("x axis passed the minimum amount. Restarting the Mars Rover App....");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+                case "E":
+                    if (x < maxX)
+                    {
+                        x++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("x axis passed the maximum amount. Restarting the Mars Rover App....");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+                case "S":
+                    if (y > 0)
+                    {
+                        y--;
+                    }
+                    else
+                    {
+                        Console.WriteLine("y axis passed the minimum amount. Restarting the Mars Rover App....");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error occured when stepping forward. The direction is : " + direction);
+                    break;
+            }
+        }
+
+        public void RunInstructions(string instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                switch (instruction)
+                {
+                    case 'L':
+                        Left();
+                        break;
+                    case 'R':
+                        Right();
+                        break;
+                    case 'M':
+                        StepForward();
+                        break;
+                    default:
+                        Console.WriteLine("Error occured running the instruction. At instruction " + instruction);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MarsRoverConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+            Console.WriteLine(" ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄▄    ▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ");
+            Console.WriteLine(" █  █▄█  █      █   ▄  █ █       █  █   ▄  █ █       █  █ █  █       █   ▄  █ ");
+            Console.WriteLine(" █       █  ▄   █  █ █ █ █  ▄▄▄▄▄█  █  █ █ █ █   ▄   █  █▄█  █    ▄▄▄█  █ █ █ ");
+            Console.WriteLine(" █       █ █▄█  █   █▄▄█▄█ █▄▄▄▄▄   █   █▄▄█▄█  █ █  █       █   █▄▄▄█   █▄▄█▄ ");
+            Console.WriteLine(" █       █      █    ▄▄  █▄▄▄▄▄  █  █    ▄▄  █  █▄█  █       █    ▄▄▄█    ▄▄  █ ");
+            Console.WriteLine(" █ ██▄██ █  ▄   █   █  █ █▄▄▄▄▄█ █  █   █  █ █       ██     ██   █▄▄▄█   █  █ █ ");
+            Console.WriteLine(" █▄█   █▄█▄█ █▄▄█▄▄▄█  █▄█▄▄▄▄▄▄▄█  █▄▄▄█  █▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█ ");
+            Console.WriteLine("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+            while (true)
+            {
+                try
+                {
+                    MarsRover rover1 = new MarsRover();
+                    MarsRover rover2 = new MarsRover();
+                    string plateau;
+
+
+                    Console.WriteLine("Please enter a valid plateau using this format ==> '5 5' or '8 8'");
+                    plateau = Console.ReadLine();
+                    rover1.InitializePlateau(plateau);
+                    rover2.InitializePlateau(plateau);
+                    Console.Clear();
+
+                    Console.WriteLine("Please initialize value for Rover Number 1");
+                    Console.WriteLine("Please enter a valid Rover position and cardinal position using this format ==> '1 2 N'");
+                    rover1.InitializeMarsRoverPos(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine("Please enter string of commands for Rover Number 1(L = LeftSpin, R = RightSpin, M = StepForward)");
+                    rover1.RunInstructions(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine("Please initialize value for Rover Number 2");
+                    Console.WriteLine("Please enter a valid Rover position and cardinal position using this format ==> '1 2 N'");
+                    rover2.InitializeMarsRoverPos(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine("Please enter string of commands for Rover Number 2(L = LeftSpin, R = RightSpin, M = StepForward)");
+                    rover2.RunInstructions(Console.ReadLine());
+                    Console.Clear();
+
+                    Console.WriteLine("Rover Number is now in position : " + rover1.x + " " + rover1.y + " " + rover1.direction);
+                    Console.WriteLine("Rover Number is now in position : " + rover2.x + " " + rover2.y + " " + rover2.direction);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An unexpected error occured. Restarting the Mars Rover App....");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    continue;
+                }
+            }
+        }
+
+    }
+}
