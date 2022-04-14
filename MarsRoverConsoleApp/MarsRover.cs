5b3b51795bfe93f3c0851999c4df4c699c876a0a
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2022-04-13 19:21:23 -0700

    add comments

--- MarsRoverConsoleApp/MarsRover.cs
+++ MarsRoverConsoleApp/MarsRover.cs
@@ -12,14 +12,21 @@ namespace MarsRoverConsoleApp
         public int maxX;
         public int maxY;
 
-        private Dictionary<string, string> cardinalDictionaryOnLeft = new Dictionary<string, string>() 
-        { 
+
+        /// <summary>
+        /// Dictionary to hold values equivalent to the supplied cardinal point value
+        /// </summary>
+        private Dictionary<string, string> cardinalDictionaryOnLeft = new Dictionary<string, string>()
+        {
             { "N", "W" },
             { "W", "S" },
             { "S", "E" },
             { "E", "N" },
         };
 
+        /// <summary>
+        /// Dictionary to hold values equivalent to the supplied cardinal point value
+        /// </summary>
         private Dictionary<string, string> cardinalDictionaryOnRight = new Dictionary<string, string>()
         {
             { "N", "E" },
@@ -28,12 +35,19 @@ namespace MarsRoverConsoleApp
             { "E", "S" },
         };
 
+        /// <summary>
+        /// This initializes the plateau for the MarsRover object
+        /// </summary>
         public void InitializePlateau(string plateau)
         {
             Int32.TryParse(plateau.Split(" ")[0], out maxX);
             Int32.TryParse(plateau.Split(" ")[1], out maxY);
         }
 
+
+        /// <summary>
+        /// This initializes the position for the MarsRover object
+        /// </summary>
         public void InitializeMarsRoverPos(string position)
         {
             if (position.Length > 0)
@@ -41,12 +55,17 @@ namespace MarsRoverConsoleApp
                 Int32.TryParse(position.Split(" ")[0], out x);
                 Int32.TryParse(position.Split(" ")[1], out y);
                 direction = position.Split(" ")[2];
-            } else
+            }
+            else
             {
                 Console.WriteLine("Unable to initialize values for MarsRover position");
             }
         }
 
+
+        /// <summary>
+        /// Changes the heading of the Rover to its right
+        /// </summary>
         public void Right()
         {
             if (cardinalDictionaryOnRight.ContainsKey(direction))
@@ -59,6 +78,9 @@ namespace MarsRoverConsoleApp
             }
         }
 
+        /// <summary>
+        /// Changes the heading of the Rover to its left
+        /// </summary>
         public void Left()
         {
             if (cardinalDictionaryOnLeft.ContainsKey(direction))
@@ -72,15 +94,18 @@ namespace MarsRoverConsoleApp
 
         }
 
+        /// <summary>
+        /// Rover moves one step forward
+        /// </summary>
         public void StepForward()
         {
             switch (direction)
             {
                 case "N":
-                    if(y < maxY)
+                    if (y < maxY) // checks if its out of bounds
                     {
                         y++;
-                    } 
+                    }
                     else
                     {
                         Console.WriteLine("y axis passed the maximum amount. Restarting the Mars Rover App....");
@@ -90,10 +115,11 @@ namespace MarsRoverConsoleApp
                     }
                     break;
                 case "W":
-                    if(x > 0)
+                    if (x > 0)  // checks if its out of bounds
                     {
                         x--;
-                    } else
+                    }
+                    else
                     {
                         Console.WriteLine("x axis passed the minimum amount. Restarting the Mars Rover App....");
                         Console.WriteLine();
@@ -102,7 +128,7 @@ namespace MarsRoverConsoleApp
                     }
                     break;
                 case "E":
-                    if (x < maxX)
+                    if (x < maxX)  // checks if its out of bounds
                     {
                         x++;
                     }
@@ -115,7 +141,7 @@ namespace MarsRoverConsoleApp
                     }
                     break;
                 case "S":
-                    if (y > 0)
+                    if (y > 0)  // checks if its out of bounds
                     {
                         y--;
                     }
@@ -133,6 +159,9 @@ namespace MarsRoverConsoleApp
             }
         }
 
+        /// <summary>
+        /// Runs the string of instructions provided.
+        /// </summary>
         public void RunInstructions(string instructions)
         {
             foreach (var instruction in instructions)
--- MarsRoverConsoleApp/Program.cs
+++ MarsRoverConsoleApp/Program.cs
@@ -20,35 +20,41 @@ namespace MarsRoverConsoleApp
             {
                 try
                 {
+                    // Initialize variables
                     MarsRover rover1 = new MarsRover();
                     MarsRover rover2 = new MarsRover();
                     string plateau;
 
-
+                    // Initialize Plateau environment for the rovers
                     Console.WriteLine("Please enter a valid plateau using this format ==> '5 5' or '8 8'");
                     plateau = Console.ReadLine();
                     rover1.InitializePlateau(plateau);
                     rover2.InitializePlateau(plateau);
                     Console.Clear();
 
-                    Console.WriteLine("Please initialize value for Rover Number 1");
+                    // Enter Position values for Rover 1.
+                    Console.WriteLine("Please enter values for Rover Number 1");
                     Console.WriteLine("Please enter a valid Rover position and cardinal position using this format ==> '1 2 N'");
                     rover1.InitializeMarsRoverPos(Console.ReadLine());
-                    Console.Clear();
+                    Console.WriteLine();
 
+                    // Enter Movement values for Rover 1.
                     Console.WriteLine("Please enter string of commands for Rover Number 1(L = LeftSpin, R = RightSpin, M = StepForward)");
                     rover1.RunInstructions(Console.ReadLine());
                     Console.Clear();
 
-                    Console.WriteLine("Please initialize value for Rover Number 2");
+                    // Enter Position values for Rover 2.
+                    Console.WriteLine("Please enter value for Rover Number 2");
                     Console.WriteLine("Please enter a valid Rover position and cardinal position using this format ==> '1 2 N'");
                     rover2.InitializeMarsRoverPos(Console.ReadLine());
-                    Console.Clear();
+                    Console.WriteLine();
 
+                    // Enter Movement values for Rover 1.
                     Console.WriteLine("Please enter string of commands for Rover Number 2(L = LeftSpin, R = RightSpin, M = StepForward)");
                     rover2.RunInstructions(Console.ReadLine());
                     Console.Clear();
 
+                    // Display new position based on input. 
                     Console.WriteLine("Rover Number is now in position : " + rover1.x + " " + rover1.y + " " + rover1.direction);
                     Console.WriteLine("Rover Number is now in position : " + rover2.x + " " + rover2.y + " " + rover2.direction);
                     Console.WriteLine();
